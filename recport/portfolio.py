12ec9fd0139a5ad7456424892ee26c1d5980e95a
harit <harit.c@outlook.com>
2020-01-31 00:41:09 +0700

    [REF] improve Portfolio, add loguru

--- recport/portfolio.py
+++ recport/portfolio.py
@@ -1,17 +1,13 @@
 from pathlib import Path
-
+from loguru import logger
 from tomlkit import comment
 from tomlkit import document
 from tomlkit import nl
 from tomlkit import table
 
-
-
-home = Path.home()
-
-
-
 class Portfolio:
+    """Portfolio class represents a real portfolio
+    """
 
     def __init__(self, port_name):
         """constructor
@@ -19,11 +15,26 @@ class Portfolio:
         Arguments:
             port_name {str} -- portfolio name
         """
+
+        file_directory = Path(Path.home() / "recport")
+        file_path = Path(file_directory / f"{port_name}.toml")
+
+        # read portfolio .toml file if exists
+        if file_path.exists():
+            # read .toml portfolio config
+            logger.debug('Pretend to read file')
+
+        # if not, create new portfolio .toml file
+        else:
+            doc = document()
+            doc.add('port_name',port_name)
+            if not file_directory.exists():
+                file_directory.mkdir(parents=True)
+            with Path(file_path).open(mode='w') as f:
+                
+                f.write(doc.as_string())
+
         self.port_name = port_name
-        doc = document()
-        doc.add('port_name',self.port_name)
-        with open(Path.joinpath(home,Path('test.toml')), 'w') as f:
-            f.write(doc.as_string())
 
 if __name__ == "__main__":
     portfolio = Portfolio('jack')
